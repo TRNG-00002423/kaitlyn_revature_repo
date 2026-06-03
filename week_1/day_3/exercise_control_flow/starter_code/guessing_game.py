7dedac9ddb54fb35446f5e7d6cb900463861271c
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2026-06-03 14:07:57 -0400

    added binary search hint to guessing game

--- week_1/day_3/exercise_control_flow/starter_code/guessing_game.py
+++ week_1/day_3/exercise_control_flow/starter_code/guessing_game.py
@@ -1,16 +1,24 @@
 import random
+import math
 
 x = random.randint(1, 100)
 guesses = 7
+right = 100
+left = 1
 
 while guesses > 0:
-    guess = int(input(f"Guess a number (guesses left: {guesses}): "))
+    binary_hint = left + (right-left)//2
+    
+    print(f"Guesses left: {guesses}")
+    guess = int(input(f"Guess a number (Binary search hint: {binary_hint}): "))
     if guess > x:
         print("Too high!")
         guesses -= 1
+        right = guess - 1
     elif guess < x:
         print("Too low!")
         guesses -=1
+        left = guess + 1
     else:
         break
 
