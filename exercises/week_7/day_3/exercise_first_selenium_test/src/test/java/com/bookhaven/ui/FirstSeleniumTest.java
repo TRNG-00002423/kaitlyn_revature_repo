1bdbe26d48575252ca2b47ea71c9ebe61ae30e90
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2026-07-20 16:58:37 -0400

    finished exercise_first_selenium_test

--- exercises/week_7/day_3/exercise_first_selenium_test/src/test/java/com/bookhaven/ui/FirstSeleniumTest.java
+++ exercises/week_7/day_3/exercise_first_selenium_test/src/test/java/com/bookhaven/ui/FirstSeleniumTest.java
@@ -2,6 +2,7 @@ package com.bookhaven.ui;
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -102,5 +103,80 @@ public class FirstSeleniumTest extends BaseTest {
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.cssSelector("button[type='submit']")).click();
+        WebElement logoutButton = driver.findElement(By.className("button"));
+        System.out.println("Found logout button: " + logoutButton.getText());
+        logoutButton.click();
+        WebElement flashMessage = driver.findElement(By.id("flash"));
+        assertTrue(flashMessage.getText().contains("You logged out of the secure area!"));
+    }
+
+    @Test
+    @DisplayName("Verify placeholder text")
+    void testPlaceholder() {
+        driver.get("https://github.com");
+        WebElement placeholder = driver.findElement(By.className("placeholder"));
+        System.out.println("Found placeholder: " + placeholder.getText());
+        assertTrue(placeholder.getText().contains("Search or jump to..."));
+    }
+
+    @Test
+    @DisplayName("Login with enter button")
+    void testLoginEnter() {
+        driver.get("https://the-internet.herokuapp.com/login");
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        username.sendKeys("tomsmith", Keys.TAB);
+        password.sendKeys("SuperSecretPassword!");
+        password.sendKeys(Keys.ENTER);
+        WebElement flashMessage = driver.findElement(By.id("flash"));
+        assertTrue(flashMessage.getText().contains("You logged into a secure area!"));
+
+    }
+
+    @Test
+    @DisplayName("Test link clicking and navigation")
+    void testLinkClicking() {
+        driver.get("https://the-internet.herokuapp.com");
+        driver.findElement(By.linkText("Checkboxes")).click();
+        assertTrue(driver.getCurrentUrl().contains("checkboxes"));
+        driver.navigate().back();
+        assertTrue(driver.getCurrentUrl().equals("https://the-internet.herokuapp.com/"));
+    }
+
+    @Test
+    @DisplayName("Test checkbox interactions")
+    void testCheckboxes() {
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        java.util.List<WebElement> checkboxes = driver.findElements(By.cssSelector("input[type='checkbox']"));
+        assertEquals(2, checkboxes.size(), "Should find 2 checkboxes");
+        WebElement checkbox1 = checkboxes.get(0);
+        WebElement checkbox2 = checkboxes.get(1);
+        assertFalse(checkbox1.isSelected());
+        checkbox1.click();
+        assertTrue(checkbox1.isSelected());
+        checkbox1.click();
+        assertFalse(checkbox1.isSelected());
+        assertTrue(checkbox2.isSelected());
+    }
+
+    @Test
+    @DisplayName("Test getting element attributes")
+    void testGetAttributes() {
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        WebElement usernameField = driver.findElement(By.id("username"));
+
+        // Get various attributes
+        String id = usernameField.getAttribute("id");
+        String type = usernameField.getAttribute("type");
+        String name = usernameField.getAttribute("name");
+
+        assertEquals("username", id);
+        assertEquals("text", type);
+        assertEquals("username", name);
+
+        // Check if element is enabled and displayed
+        assertTrue(usernameField.isEnabled());
+        assertTrue(usernameField.isDisplayed());
     }
 }
